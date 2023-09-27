58f2ebd797163c8dd701d9bb31aae9fd35dc1a43
Rysth <johnpalacios.t@gmail.com>
2023-09-27 16:19:30 -0500

    Fix Rubocop errors.

--- methods.rb
+++ methods.rb
@@ -1,96 +1,58 @@
-$morse_dict = {
-  "a" => ".-",
-  "b" => "-...",
-  "c" => "-.-.",
-  "d" => "-..",
-  "e" => ".",
-  "f" => "..-.",
-  "g" => "--.",
-  "h" => "....",
-  "i" => "..",
-  "j" => ".---",
-  "k" => "-.-",
-  "l" => ".-..",
-  "m" => "--",
-  "n" => "-.",
-  "o" => "---",
-  "p" => ".--.",
-  "q" => "--.-",
-  "r" => ".-.",
-  "s" => "...",
-  "t" => "-",
-  "u" => "..-",
-  "v" => "...-",
-  "w" => ".--",
-  "x" => "-..-",
-  "y" => "-.--",
-  "z" => "--..",
-  " " => " ",
-  "1" => ".----",
-  "2" => "..---",
-  "3" => "...--",
-  "4" => "....-",
-  "5" => ".....",
-  "6" => "-....",
-  "7" => "--...",
-  "8" => "---..",
-  "9" => "----.",
-  "0" => "-----"
-}
-
-=begin 
-  Description:
-    -morseCode: A string representation of a morse code value.
-    -pattern: Pattern used to split the words/message by the spaces added to them.
-    -decodeMethod: Is the method used to reuse the decode_char and decode_word methods.
-=end
-def decode_loop(morseCode, pattern, decodeMethod)
-  decodedMessage = ""
-  stringArray = morseCode.split(pattern)
-  stringArray.each do |n|
-    if pattern 
-      decodedMessage.concat(decodeMethod.(n).concat(" "))
+#
+#   Description:
+#     -morse_code: A string representation of a morse code value.
+#     -pattern: Pattern used to split the words/message by the spaces added to them.
+#     -decoded_method: Is the method used to reuse the decode_char and decode_word methods.
+def decode_loop(morse_code, pattern, decoded_method)
+  decoded_message = ''
+  string_array = morse_code.split(pattern)
+  string_array.each do |n|
+    if pattern
+      decoded_message.concat(decoded_method.call(n).concat(' '))
     else
-      decodedMessage.concat(decodeMethod.(n))
+      decoded_message.concat(decoded_method.call(n))
     end
   end
-  decodedMessage
+  decoded_message
 end
 
-=begin 
-  Description: It returns a letter in UPPERCASE by passing a morse code word to it.
-=end
+#
+#   Description: It returns a letter in UPPERCASE by passing a morse code word to it.
 def decode_char(morse)
-  uppercaseLetter = ""
+  uppercase_letter = ''
+  morse_dict = { 'a' => '.-', 'b' => '-...', 'c' => '-.-.', 'd' => '-..', 'e' => '.', 'f' => '..-.', 'g' => '--.',
+                 'h' => '....', 'i' => '..', 'j' => '.---', 'k' => '-.-', 'l' => '.-..', 'm' => '--', 'n' => '-.',
+                 'o' => '---', 'p' => '.--.', 'q' => '--.-', 'r' => '.-.', 's' => '...', 't' => '-', 'u' => '..-',
+                 'v' => '...-', 'w' => '.--', 'x' => '-..-', 'y' => '-.--', 'z' => '--..', ' ' => ' ', '1' => '.----',
+                 '2' => '..---', '3' => '...--', '4' => '....-', '5' => '.....', '6' => '-....', '7' => '--...',
+                 '8' => '---..', '9' => '----.', '0' => '-----' }
 
-  $morse_dict.each do |key, value|
+  morse_dict.each do |key, value|
     next unless morse == value
-    uppercaseLetter = key.upcase
-    break 
+
+    uppercase_letter = key.upcase
+    break
   end
 
-   uppercaseLetter
+  uppercase_letter
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
 
-
-=begin 
-  Description: Calls the decode_loop method to return a whole WORD by using the decode_char method created before.
-=end
+#
+#   Description: Calls the decode_loop method to return a whole WORD by using the decode_char method created before.
 def decode_word(word)
   decode_loop(word, nil, method(:decode_char))
 end
 
-puts decode_word("-- -.--")
+puts decode_word('-- -.--')
 
-=begin 
-  Description: Calls the decode_loop method to return a whole MESSAGE by using the decode_char method created before.
-=end
+#
+#   Description: Calls the decode_loop method to return a whole MESSAGE by using the decode_char method created before.
 def decode(string)
   decode_loop(string, /   /, method(:decode_word))
 end
 
-puts decode("-- -.--   -. .- -- .")
+puts decode('-- -.--   -. .- -- .')
 
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
